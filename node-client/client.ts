6ab403f5dd79198c37c4549f66b4d3980f725624
Vlad <vlad@Vlads-MacBook-Pro.local>
2020-07-10 17:55:59 +0300

    Added streaming for nodejs

--- node-client/client.ts
+++ node-client/client.ts
@@ -13,12 +13,93 @@ const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
 });
 const randomProto: any = grpc.loadPackageDefinition(packageDefinition).random;
 
-function getRundomNumber(client) {
-  client.generateRandomNumber({maxLength: process.argv[2]}, function (err, response) {
-    if (err) {
-      console.error(err);
+async function getRundomNumber(client) {
+  return new Promise((resolve, reject) => {
+    client.generateRandomNumber({maxLength: process.argv[2]}, function (err, response) {
+      if (err) {
+        console.error(err);
+        reject(err);
+      }
+      console.log("Random number: ", response.number);
+      resolve();
+    });
+  });
+}
+
+async function streamNumbers(client) {
+  return new Promise((resolve, reject) => {
+    var call = client.streamNumbers({maxLength: process.argv[2]});
+
+    call.on("data", function ({number}) {
+      console.log(`streamNumbers got number: ${number}`);
+    });
+    call.on("end", function () {
+      console.log(`streamNumbers successfully finished`);
+      resolve();
+    });
+    call.on("error", function (e) {
+      console.log(`streamNumbers error ${e}`);
+      reject(e);
+    });
+    call.on("status", function (status) {
+      console.log(`streamNumbers status ${status}`);
+    });
+  });
+}
+
+async function logStreamOfRandomNumbers(client) {
+  return new Promise(async (resolve, reject) => {
+    var call = client.logStreamOfRandomNumbers(function (error, {message}) {
+      if (error) {
+        console.log(`logStreamOfRandomNumbers error ${error}`);
+        reject(error);
+      }
+      console.log(`logStreamOfRandomNumbers successfully finished`);
+      resolve(message);
+    });
+
+    for (let i = 0; i < 2; ++i) {
+      const number = Math.floor(Math.random() * Math.floor(200));
+      await call.write({number});
     }
-    console.log("Random number: ", response.number);
+
+    call.end();
+  });
+}
+
+async function bidirectionalStream(client) {
+  return new Promise(async (resolve, reject) => {
+    var call = client.bidirectionalStream(function (error, {message}) {
+      if (error) {
+        console.log(`bidirectionalStream error ${error}`);
+        reject(error);
+      }
+      console.log(`bidirectionalStream successfully finished`);
+      resolve(message);
+    });
+
+    call.on("data", function ({number}) {
+      console.log(`bidirectionalStream got number: ${number}`);
+    });
+    call.on("end", function () {
+      console.log(`bidirectionalStream successfully finished`);
+      resolve();
+    });
+    call.on("error", function (e) {
+      console.log(`bidirectionalStream error ${e}`);
+      reject(e);
+    });
+    call.on("status", function (status) {
+      console.log(`bidirectionalStream status:`);
+      console.dir(status);
+    });
+
+    for (let i = 0; i < 2; ++i) {
+      const number = Math.floor(Math.random() * Math.floor(200));
+      await call.write({number});
+    }
+
+    call.end();
   });
 }
 
@@ -28,7 +109,12 @@ function main() {
 
   var client = new randomProto.Random(address, grpc.credentials.createInsecure());
 
-  setInterval(() => getRundomNumber(client), 1000);
+  setInterval(async () => {
+    await getRundomNumber(client);
+    await streamNumbers(client);
+    await logStreamOfRandomNumbers(client);
+    await bidirectionalStream(client);
+  }, 1000);
 }
 
 main();
--- node-server/server.ts
+++ node-server/server.ts
@@ -27,12 +27,65 @@ function generateRandomNumber(call, callback) {
 }
 
 /**
+ * Implements the streamNumbers RPC method.
+ */
+function streamNumbers(call) {
+  const {maxLength} = call.request;
+  const number = Math.floor(Math.random() * Math.floor(maxLength));
+
+  console.info(`streamNumbers method received request. Max number Length = ${maxLength}`);
+  console.info(`streaming data...`);
+
+  for (let i = 0; i < 2; ++i) {
+    call.write({number});
+  }
+  call.end();
+}
+
+/**
+ * Implements the logStreamOfRandomNumbers RPC method.
+ */
+function logStreamOfRandomNumbers(call, callback) {
+  call.on("data", function (number) {
+    console.info(`logStreamOfRandomNumbers got number: ${number}`);
+  });
+  call.on("end", function () {
+    console.info(`logStreamOfRandomNumbers finished`);
+    callback(null, {message: "Successfully logged all numbers"});
+  });
+}
+
+/**
+ * Implements the bidirectionalStream RPC method.
+ */
+function bidirectionalStream(call) {
+  call.on("data", function ({number}) {
+    const resNumber = Math.random() * 100;
+
+    console.info(`bidirectionalStream got number: ${number}`);
+    console.info(`bidirectionalStream pushed number: ${resNumber}`);
+
+    call.write({number: resNumber});
+  });
+  call.on("end", function () {
+    console.info(`bidirectionalStream finished`);
+    call.end();
+  });
+}
+
+/**
  * Starts an RPC server that receives requests for the Greeter service at the
  * sample server port
  */
 function main() {
   const server = new grpc.Server();
-  server.addService(randomProto.Random.service, {generateRandomNumber: generateRandomNumber});
+  server.addService(randomProto.Random.service, {
+    generateRandomNumber,
+    streamNumbers,
+    logStreamOfRandomNumbers,
+    bidirectionalStream,
+  });
+
   server.bind("0.0.0.0:50052", grpc.ServerCredentials.createInsecure());
   server.start();
   console.info("nodejs grpc service started on localhost:50052");
